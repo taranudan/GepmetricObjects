96e7575ea818698f9b1edcc804ad3e19a989398b
taranudan <88153465+taranudan@users.noreply.github.com>
2021-08-24 19:31:29 +0300

    Update Application.java

--- Application.java
+++ Application.java
@@ -7,7 +7,7 @@ public class Application {
 	public static void main(String[] args) {
 
 
-		Line short_horizontal_line = new Line( 100,0, 200,0 );
+	      Line short_horizontal_line = new Line( 100,0, 200,0 );
 	      short_horizontal_line.printCoords();
 	      System.out.println( short_horizontal_line.length() );
 	      
