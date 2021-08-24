efdeb2430780fef9f8aecf43c3b96652bfe37af4
taranudan <88153465+taranudan@users.noreply.github.com>
2021-08-24 19:30:50 +0300

    Add files via upload

---
+++ Line.java
@@ -0,0 +1,42 @@
+package geometry;
+
+import java.lang.Math;
+
+public class Line {
+
+	 // properties
+	  public int startX;
+	  public int endX;
+	  public int startY;
+	  public int endY;
+
+
+	  // constructor
+	  public Line(int startX,int startY,int endX,int endY) {
+		  
+		if ((startX == endX) && (startY == endY)) {
+			System.err.println("The coordinates of both ends of the line are the same. This is not a line but a point.");
+		}
+		else {
+			this.startY = startY;
+		    this.startX = startX;
+		    this.endX   = endX;
+		    this.endY   = endY;
+		}
+	    
+	  } 
+	  
+	  public void printCoords() {
+		  System.out.printf("[%d:%d]----->[%d:%d]\n", startX, startY, endX, endY);
+	  }
+	  
+	  public int length() {
+		  
+		  int lineLenth;
+		  
+		  lineLenth = (int) Math.sqrt(Math.pow(endX - startX, 2) + Math.pow(endY - startY, 2));
+		  
+		  return lineLenth;
+	  }
+
+}
